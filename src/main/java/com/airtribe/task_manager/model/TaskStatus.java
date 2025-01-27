7184c7d55f4004b13eaafc6c82f1f5c4ef4a3caa
sarahfatima1298 <sarahfatima1298@gmail.com>
2025-01-27 21:51:13 +0530

    Airtribe: Task Manager

--- src/main/java/com/airtribe/task_manager/model/TaskStatus.java
+++
@@ -1,8 +0,0 @@
-package com.airtribe.task_manager.model;
-
-public enum TaskStatus {
-    PENDING,
-    IN_PROGRESS,
-    COMPLETED,
-    ON_HOLD
-}
--- src/main/java/com/airtribe/task_manager/repository/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.airtribe.task_manager.repository;
-
-
-import com.airtribe.task_manager.model.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-    // Additional query methods, if needed
-}
